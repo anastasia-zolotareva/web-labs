ab83b89358da8384f3f1927edbf1ea4b6205aded
Anastasia <nastyazolotareva199@gmail.com>
2022-03-28 20:15:08 +0300

    6.1

--- style.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    var cur_item = document.getElementsByClassName("item");
-    for (let i = 0; i < document.getElementsByClassName('item').length; i++) {
-        var cur_item = document.getElementsByClassName('item');
-        cur_item[i].addEventListener("click", function() {
-        cur_item[i].className = "active_item";});
-    }
-})();
