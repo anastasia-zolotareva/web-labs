b7ce845d50eed677bb9b9f6a7556afc9d2c4862c
Anastasia <nastyazolotareva199@gmail.com>
2022-09-07 14:02:57 +0300

    last v 2022

--- src/js/account.js
+++ src/js/account.js
@@ -1,48 +1,284 @@
-function loadPage(userPageUrl) {
-    document.getElementsByClassName("warning")[0].className = "warning hide";
-    document.getElementsByClassName("preloader")[0].className = "preloader";
-
-    fetch(userPageUrl)
-        .then(response => response.json())
-        .then(userInfo => {
-            var userName = String(userInfo.name) + " (" + String(userInfo.username) + ")";
-            console.log(userName);
-            var userAddress = String(userInfo.address.city) 
-                + " " + String(userInfo.address.street) 
-                + " " + String(userInfo.address.suite)
-                + " " + String(userInfo.address.zipcode);
-            console.log(userAddress);
-            var userPhone = String(userInfo.phone);
-
-            console.log(userPhone);
-
-            var print_block = document.getElementsByClassName("user_info");
-            print_block[0].innerHTML = "<h3>Имя</h3><p>" + userName + "</p>" + 
-                "<h3>Адрес</h3><p>" + userAddress + "</p><h3>Телефон</h3><p>" +
-                userPhone + "</p>";
-            console.log("html added");
-                
-        })
-        .then(() => {
-            console.log("try to show");
-            document.getElementsByClassName("preloader")[0].className = "preloader hide";
-            document.getElementsByClassName("user_info")[0].className = "user_info";
-        })
-        .catch(() => {
-            console.log("smth went wrong");
-            document.getElementsByClassName("preloader")[0].className = "preloader hide";
-            document.getElementsByClassName("warning")[0].className = "warning";
-        });  
+class User {
+    constructor() {
+        this.authorised = false;
+    }
+    clear() {
+        this.id = undefined;
+        this.name = undefined;
+        this.userName = undefined;
+        this.addressCity = undefined;
+        this.addressStreet = undefined;
+        this.addressSuite = undefined;
+        this.addressZipcode = undefined;
+        this.phone = undefined;
+        this.authorised = false;
+    }
+    async load(userPageURL) {
+        if (this.authorised == true) {
+            throw Error("user is already authorised");
+        }
+        await fetch(userPageURL)
+            .then(response => response.json())
+            .then(user => {
+                this.name = String(user.name);
+                this.userName = String(user.username);
+                this.addressCity = String(user.address.city);
+                this.addressStreet = String(user.address.street);
+                this.addressSuite = String(user.address.suite);
+                this.addressZipcode = String(user.address.zipcode);
+                this.phone = String(user.phone);
+                this.authorised = true;
+                console.log("loaded user info");
+            })
+            .catch(() => {
+                throw Error("smth went wrong while load");
+            });
+    }
+    setInfo(id,
+            name, 
+            userName, 
+            addressCity,
+            addressStreet, 
+            addressSuite, 
+            addressZipcode,
+            phone) {
+        this.id = id;
+        this.name = name;
+        this.userName = userName;
+        this.addressCity = addressCity;
+        this.addressStreet = addressStreet;
+        this.addressSuite = addressSuite;
+        this.addressZipcode = addressZipcode;
+        this.phone = phone;
+        this.authorised = true;
+    }
 }
 
-function showAccount() {
-    var userId = document.getElementById("userId");
+class UserCardCreater {
+    constructor() {
+    }
+    create(curUser) {
+        if (curUser.authorised == false) {
+            throw Error("cant create non authorised user");
+        }
+        let curUserCard = document.createElement("section");
+        curUserCard.className = "user_card";
+            
+        curUserCard.innerHTML = "<h3>Имя</h3><p>" + curUser.name + " (" +
+            curUser.userName + ")</p>" +
+            "<h3>Адрес</h3><p>" + curUser.addressCity + " " +
+            curUser.addressStreet + " " +
+            curUser.addressSuite + " " +
+            curUser.addressZipcode +
+            "</p><h3>Телефон</h3><p>" + curUser.phone + "</p>";
+        return curUserCard;
+    }
+}
+
+class UserCardPresenter {
+    constructor(user, userCardCreater, printBlockClassName) {
+        this.curUser = user;
+        this.userCardCreater = userCardCreater;
+        this.printBlock = document.getElementsByClassName(printBlockClassName)[0];
+        this.printBlockClassName = printBlockClassName;
+    }
+    showUserCard() {
+        console.log("create user card");
+        let domObject = this.userCardCreater.create(this.curUser);
+
+        this.printBlock.appendChild(domObject);
+    
+        this.printBlock.className = this.printBlockClassName;
+    }
+    hideUserCard() {
+        console.log("hide user card");
+    
+        document.getElementsByClassName('user_card')[0].remove();
+        this.printBlock.className = `${this.printBlockClass} hide`;
+    }
+}
+class PageFieldsControler {
+    constructor(user, singInFieldName, singOutFieldName) {
+        this.user = user;
+        this.domSingIn = document.getElementById(singInFieldName);
+        this.domSingOut = document.getElementById(singOutFieldName);
+    }
+    changeToSingOut() {
+        document.getElementById('userId').className = "hide";
+        document.getElementById('userPassword').className = "hide";
+        this.domSingIn.className = "hide";
+        this.domSingOut.className = "";
+    }
+    changeToSingIn() {
+        document.getElementById('userId').className = "";
+        document.getElementById('userPassword').className = "";
+        this.domSingIn.className = "";
+        this.domSingOut.className = "hide";
+    }
+    changeFields() {
+        if (this.user.authorised == true) {
+            this.changeToSingOut();
+        } else {
+            this.changeToSingIn();
+        }
+    }
+}
+
+class LoadingProcessPresenter {
+    constructor(warningClassName, preloaderClassName) {
+        this.warningClassName = warningClassName;
+        this.warningClass = document.getElementsByClassName(warningClassName)[0];
+        this.preloaderClassName = preloaderClassName;
+        this.preloaderClass = document.getElementsByClassName(preloaderClassName)[0];
+    }
+    showPreloader() {
+        this.warningClass.className = `${this.warningClassName} hide`;
+        this.preloaderClass.className = `${this.preloaderClassName}`;
+    }
+    showWarning() {
+        this.warningClass.className = `${this.warningClassName}`;
+        this.preloaderClass.className = `${this.preloaderClassName} hide`;
+    }
+    hideAll() {
+        this.warningClass.className = `${this.warningClassName} hide`;
+        this.preloaderClass.className = `${this.preloaderClassName} hide`;
+    }
+}
+
+class UserStorage {
+    constructor(curUser, nameInStorage) {
+        this.user = curUser;
+        this.nameInStorage = nameInStorage;
+    }
+    isSingIn() {
+        let curUser = JSON.parse(localStorage.getItem(this.nameInStorage));
+        if (curUser) {
+            return true;
+        }
+        return false;
+    }
+    load() {
+        if (!this.isSingIn()) {
+            throw Error("you are trying to load unexisting info");
+        }
+        let curUser = JSON.parse(localStorage.getItem(this.nameInStorage));
+        this.user.setInfo(
+            curUser.id,
+            curUser.name,
+            curUser.userName,
+            curUser.addressCity,
+            curUser.addressStreet,
+            curUser.addressSuite,
+            curUser.addressZipcode,
+            curUser.phone
+        );
+    }
+    save() {
+        if (!this.user) {
+            throw Error("you are trying to save unexisting user");
+        }
+        let curUser = {
+            id : this.user.id,
+            name : this.user.name,
+            userName : this.user.userName,
+            addressCity : this.user.addressCity,
+            addressStreet : this.user.addressStreet,
+            addressSuite : this.user.addressSuite,
+            addressZipcode : this.user.addressZipcode,
+            phone : this.user.phone
+        }
+        localStorage.setItem(this.nameInStorage, JSON.stringify(curUser));
+    }
+    clear() {
+        if (!this.isSingIn()) {
+            throw Error("you are trying to clear unexisting info");
+        }
+        localStorage.removeItem(this.nameInStorage);
+    }
+}
+let user;
+let userCardPresenter;
+let pageFieldsControler;
+let loadingProcessPresenter;
+let userStorage;
+
+function initialiseClasses() {
+    console.log("initialise classes");
+    user = new User();
+    userCardPresenter = new UserCardPresenter(user, new UserCardCreater(), "items_board");
+    pageFieldsControler = new PageFieldsControler(user, "user_singin", "user_singout");
+    loadingProcessPresenter = new LoadingProcessPresenter("warning", "preloader");
+    userStorage = new UserStorage(user, "curUser");
+}
+
+window.addEventListener('load', function() {
+    console.log("load page");
+    initialiseClasses();
+    if (userStorage.isSingIn()) {
+        userStorage.load();
+        pageFieldsControler.changeFields();
+        userCardPresenter.showUserCard();
+    }
+});
+
+async function loadUserCard(userUrl) {
+    loadingProcessPresenter.showPreloader();
+
+    try {
+        await user.load(userUrl);
+
+        userCardPresenter.showUserCard();
+
+        pageFieldsControler.changeFields();
+
+        console.log("try to show");
+
+        userStorage.save();
+
+        loadingProcessPresenter.hideAll();
+    }
+    catch(error) {
+        console.log("smth went wrong");
+        console.log(error);
+
+        loadingProcessPresenter.showWarning();
+
+        userStorage.clear();
+        user.clear();
+    }
+}
+
+async function singIn() {
+    let userId = document.getElementById("userId");
+    let userPassword = document.getElementById("userPassword");
+
     if (userId.value == "") {
         var userPage = document.getElementsByClassName("user_info");
+
         userPage[0].className = "user_info";
         userPage[0].innerHTML = "<h3>Вы не ввели id</h3>";
     } else {
-        const userUrl = 'http://jsonplaceholder.typicode.com/users/' + String(userId.value);
-        loadPage(userUrl);
+        if (userPassword.value == "12345") {
+            const userUrl = `http://jsonplaceholder.typicode.com/users/${String(userId.value)}`;
+
+            await loadUserCard(userUrl);
+        } else {
+            var userPage = document.getElementsByClassName("user_info");
+
+            userPage[0].className = "user_info";
+            userPage[0].innerHTML = "<h3>Неверный пароль</h3>";
+        }
     }
+}
+
+function singOut() {
+    if (user.authorised == false) {
+        throw Error("user is not authorised");
+    }
+    localStorage.clear();
+    user.clear();
+
+    pageFieldsControler.changeFields();
+
+    userCardPresenter.hideUserCard();
 }
--- src/js/navigation_scr.js
+++ src/js/navigation_scr.js
@@ -1,9 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
     let this_url = document.URL;
-    for (let i = 0; i < document.getElementsByClassName('nav_element').length; i++) {
-        if (document.getElementsByClassName('nav_element')[i]["href"] === this_url) {
-            active_element = document.getElementsByClassName('nav_element')[i];
-            active_element.className = 'current_nav_element';
+    nav_element = document.getElementsByClassName('nav_element');
+    user_nav_element = document.getElementsByClassName('user_nav_element');
+    for (let i = 0; i < nav_element.length; i++) {
+        if (nav_element[i]["href"] === this_url) {
+            active_element = nav_element[i];
+            active_element.className = 'nav_element current_nav_element';
+            active_element.removeAttribute('href');
+            break;
+        }
+    }
+    for (let i = 0; i < user_nav_element.length; i++) {
+        if (user_nav_element[i]["href"] === this_url) {
+            active_element = user_nav_element[i];
+            active_element.className = 'user_nav_element current_user_nav_element';
             active_element.removeAttribute('href');
             break;
         }
